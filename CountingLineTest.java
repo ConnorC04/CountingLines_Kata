4bb2b66c6ad6efd783f4e278bb1e0cc868c0f838
ConnorC04 <connorjcasey@iCloud.com>
2025-04-16 10:08:26 -0400

    his favorite color is green trees

--- CountingLineTest.java
+++ CountingLineTest.java
@@ -3,7 +3,32 @@ import org.junit.jupiter.api.Test;
 public class CountingLineTest {
 
     @Test
-    public void testCountLines(){
+    public void testCountLine(){
+        int lineCount = 0;
+        String codeToRead = "";
+    }
 
+    @Test
+    public void testCountMultipleLines(){
+        int lineCount = 0;
+        String codeToRead = "";
+    }
+
+    @Test
+    public void testSingleLineComment(){
+        int lineCount = 0;
+        String codeToRead = "";
+    }
+
+    @Test
+    public void testCommentBlock(){
+        int lineCount = 0;
+        String codeToRead = "";
+    }
+
+    @Test
+    public void testInLineComment(){
+        int lineCount = 0;
+        String codeToRead = "";
     }
 }
---
+++ out/production/CountingLines_Kata/README.md
@@ -0,0 +1,1 @@
+# CountingLines_Kata
