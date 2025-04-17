8fd42833f268a13b3849c6376ea25a4354baa855
ConnorC04 <connorjcasey@iCloud.com>
2025-04-17 08:54:32 -0400

    what is this

--- CountingLines.java
+++ CountingLines.java
@@ -9,9 +9,10 @@ public class CountingLines {
         String[] separateLines = javaCode.split("\n");
         for (String line : separateLines){
             String[] commentBlockStart = line.split("/*");
+            if (commentBlockStart.length == 2){
+                String[] commentBlockEnd = line.split("/*/");
+            }
         }
         return lineCount;
     }
-
-
 }
